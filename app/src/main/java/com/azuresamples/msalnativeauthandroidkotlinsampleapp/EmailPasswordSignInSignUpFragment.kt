b73803dc88d6258880209169e5b7c716297cdc84
Yuki-YuXin <111055878+Yuki-YuXin@users.noreply.github.com>
2024-02-26 21:28:00 +0000

    variable naming : accessTokenState -> accessTokenResult (#16)

--- app/src/main/java/com/azuresamples/msalnativeauthandroidkotlinsampleapp/EmailPasswordSignInSignUpFragment.kt
+++ app/src/main/java/com/azuresamples/msalnativeauthandroidkotlinsampleapp/EmailPasswordSignInSignUpFragment.kt
@@ -250,9 +250,9 @@ class EmailPasswordSignInSignUpFragment : Fragment() {
 
     private fun displayAccount(accountState: AccountState) {
         CoroutineScope(Dispatchers.Main).launch {
-            val accessTokenState = accountState.getAccessToken()
-            if (accessTokenState is GetAccessTokenResult.Complete) {
-                val accessToken = accessTokenState.resultValue.accessToken
+            val accessTokenResult = accountState.getAccessToken()
+            if (accessTokenResult is GetAccessTokenResult.Complete) {
+                val accessToken = accessTokenResult.resultValue.accessToken
                 binding.resultAccessToken.text =
                     getString(R.string.result_access_token_text) + accessToken
 
