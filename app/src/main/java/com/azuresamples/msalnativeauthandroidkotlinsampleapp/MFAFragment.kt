b95a83eb594dbf985a60fbf9e1595ac9a8ce2cbf
yuxin <yuxin@microsoft.com>
2025-05-02 17:10:55 +0100

    Use the correct import reference

--- app/src/main/java/com/azuresamples/msalnativeauthandroidkotlinsampleapp/MFAFragment.kt
+++ app/src/main/java/com/azuresamples/msalnativeauthandroidkotlinsampleapp/MFAFragment.kt
@@ -1,14 +1,13 @@
 package com.azuresamples.msalnativeauthandroidkotlinsampleapp
 
 import android.app.AlertDialog
-import android.app.Fragment
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.appcompat.app.AppCompatActivity
 import com.azuresamples.msalnativeauthandroidkotlinsampleapp.databinding.FragmentEmailPasswordBinding
 import com.microsoft.identity.common.java.util.StringUtil
 import com.microsoft.identity.nativeauth.AuthMethod
