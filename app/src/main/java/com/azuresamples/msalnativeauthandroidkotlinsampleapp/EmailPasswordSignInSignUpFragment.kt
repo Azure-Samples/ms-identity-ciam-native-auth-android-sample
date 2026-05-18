5a79e95aac2a5205170c2c63f9d07d95fb769455
Silviu Petrescu <spetrescu@microsoft.com>
2026-05-18 12:16:00 +0100

    PR comment

--- app/src/main/java/com/azuresamples/msalnativeauthandroidkotlinsampleapp/EmailPasswordSignInSignUpFragment.kt
+++ app/src/main/java/com/azuresamples/msalnativeauthandroidkotlinsampleapp/EmailPasswordSignInSignUpFragment.kt
@@ -46,7 +46,7 @@ class EmailPasswordSignInSignUpFragment : Fragment(), NativeAuthRequestIntercept
     override fun additionalHeaders(requestUrl: URL): Map<String, String>? {
         if (requestUrl.path.contains("oauth2/v2.0/initiate")) {
             return mapOf(
-                "value_1" to "customer_header_1",              // Will be ignored: doesn't start with "x-"
+                "value_1" to "ignored-custom-header",          // Will be ignored: doesn't start with "x-"
                 "x-client-header" to "customer_header_2",      // Will be ignored: starts with reserved prefix "x-client-"
                 "X-my-custom-header" to "my data"              // Will be added to the network request
             )
